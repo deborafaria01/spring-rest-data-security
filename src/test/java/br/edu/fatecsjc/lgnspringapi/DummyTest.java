c6593886a39e551fe8fc02544da0289a3411236f
deborafaria01 <deeborafaria@gmail.com>
2024-11-16 21:14:27 -0300

    test: add basic dummy test to pass unit-test stage

---
+++ src/test/java/br/edu/fatecsjc/lgnspringapi/DummyTest.java
@@ -0,0 +1,13 @@
+package br.edu.fatecsjc.lgnspringapi;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DummyTest {
+
+    @Test
+    void basicTest() {
+        assertTrue(true, "This is a dummy test.");
+    }
+}

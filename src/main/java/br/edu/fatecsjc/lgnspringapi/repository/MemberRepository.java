ada2ae543e93a2a5759f1d2fd34519e43ee5422b
deborafaria01 <deeborafaria@gmail.com>
2024-11-24 18:13:49 -0300

    Feat: Update repository interfaces for Member

--- src/main/java/br/edu/fatecsjc/lgnspringapi/repository/MemberRepository.java
+++ src/main/java/br/edu/fatecsjc/lgnspringapi/repository/MemberRepository.java
@@ -3,9 +3,16 @@ package br.edu.fatecsjc.lgnspringapi.repository;
 import br.edu.fatecsjc.lgnspringapi.entity.Group;
 import br.edu.fatecsjc.lgnspringapi.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    void deleteMembersByGroup(Group group);
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Member m WHERE m.group = :group")
+    void deleteMembersByGroup(@Param("group") Group group);
 }
